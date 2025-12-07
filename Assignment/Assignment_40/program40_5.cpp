6265b424ce30a016f1ace3f1465c84d9d53083f1
Akshata Deole <deoleakshata@gmail.com>
2025-12-08 00:43:59 +0530

    Add files via upload

---
+++ Assignment/Assignment_40/program40_5.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+void Display(int no)
+{
+     int iCnt = 0;
+     char ch = 'a';
+
+    for(iCnt = 1; iCnt <= no; iCnt++)
+    {
+        cout << " %c\t " << ch;
+        iCnt++;
+    }
+
+
+    
+}
+
+int main()
+{
+
+    int iValue = 0;
+
+    printf("Enter a number: ");
+    scanf("%d", &iValue);
+
+    Display(iValue);
+    return 0;
+}
